50bc40c41097191fe478ea143054bed7119def00
Logan Rice <loganrice72@gmail.com>
2019-11-11 21:34:26 -0800

    allow cors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
 
   def create
     token = Rails.application.credentials[:api_token]
